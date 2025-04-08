37169d56364e0b503170540daaf86f71e485ce53
John Maus <mauscomp@live.com>
2025-04-07 22:15:38 -0500

    Started creating the project options form fields.

--- crit-web/src/pages/Project/project-options.page.tsx
+++ crit-web/src/pages/Project/project-options.page.tsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import { GetEnvValues } from "../../constants/environment";
 import { Project } from "../../models/project.model";
 
+import "./projects.scss";
+
 async function getProject(projectId: string | undefined): Promise<Project | null> {
     return new Promise(async (resolve, reject): Promise<void> => {
         const response = await fetch(new URL(`${GetEnvValues()?.critApiUrl}/Project/${projectId}`), {
@@ -43,7 +45,18 @@ function ProjectOptions(): JSX.Element {
             <h2>{project?.name ?? '<no project name>'}</h2>
             <h3>Options</h3>
             <hr />
-            {project && (null)}
+            {project && (
+                <div className="project-options">
+                    <div className="project-option">
+                        <label htmlFor="project-name">Project Name</label>
+                        <input type="text" id="project-name" value={project.name} readOnly />
+                    </div>
+                    <div className="project-option">
+                        <label htmlFor="project-description">Project Description</label>
+                        <textarea id="project-description" value={project.description} readOnly />
+                    </div>
+                </div>
+            )}
         </>
     );
 }
